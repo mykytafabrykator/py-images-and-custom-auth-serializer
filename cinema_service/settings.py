673ce2a05d8b746a1f2e21966e681f4dd6d723cd
Mykyta Fabrykator <dk01.fabrikator@lll.kpi.ua>
2025-02-14 17:06:28 +0200

    Update settings, add static to urls

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -137,3 +137,16 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / "/media/"
+
+MEDIA_URL = "/media/"
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
+    ],
+}
